86cc1d7a0b5ec099f6a9ff25e0b169aee9879b67
Ben Metzger and Terry Urich <ben@>
2017-03-21 14:06:20 -0700

    add working flash cards for 9 concepts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,82 @@
 $(document).ready(function() {
-  $(".panel-body").click(function() {
-    $("h1").addClass("green-header")
-  }
-}
+  $("#js-title").click(function() {
+    $("#js-title").hide();
+    $("#js-body").show();
+  });
+  $("#js-body").click(function() {
+    $("#js-title").show();
+    $("#js-body").hide();
+  });
+
+  $("#op-title").click(function() {
+    $("#op-title").hide();
+    $("#op-body").show();
+  });
+  $("#op-body").click(function() {
+    $("#op-title").show();
+    $("#op-body").hide();
+  });
+
+  $("#va-title").click(function() {
+    $("#va-title").hide();
+    $("#va-body").show();
+  });
+  $("#va-body").click(function() {
+    $("#va-title").show();
+    $("#va-body").hide();
+  });
+
+  $("#na-title").click(function() {
+    $("#na-title").hide();
+    $("#na-body").show();
+  });
+  $("#na-body").click(function() {
+    $("#na-title").show();
+    $("#na-body").hide();
+  });
+
+  $("#fu-title").click(function() {
+    $("#fu-title").hide();
+    $("#fu-body").show();
+  });
+  $("#fu-body").click(function() {
+    $("#fu-title").show();
+    $("#fu-body").hide();
+  });
+
+  $("#me-title").click(function() {
+    $("#me-title").hide();
+    $("#me-body").show();
+  });
+  $("#me-body").click(function() {
+    $("#me-title").show();
+    $("#me-body").hide();
+  });
+
+  $("#ar-title").click(function() {
+    $("#ar-title").hide();
+    $("#ar-body").show();
+  });
+  $("#ar-body").click(function() {
+    $("#ar-title").show();
+    $("#ar-body").hide();
+  });
+
+  $("#pa-title").click(function() {
+    $("#pa-title").hide();
+    $("#pa-body").show();
+  });
+  $("#pa-body").click(function() {
+    $("#pa-title").show();
+    $("#pa-body").hide();
+  });
+
+  $("#re-title").click(function() {
+    $("#re-title").hide();
+    $("#re-body").show();
+  });
+  $("#re-body").click(function() {
+    $("#re-title").show();
+    $("#re-body").hide();
+  });
+});
