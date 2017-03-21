1ea7434b3a61e2153a00e98d6333daaba9500b70
Ben Metzger and Terry Urich <ben@>
2017-03-21 16:17:27 -0700

    changed flashcard function to not use unique ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,15 @@
 $(document).ready(function() {
-  $("#js-title").click(function() {
-    $("#js-title").hide();
-    $("#js-body").show();
-  });
-  $("#js-body").click(function() {
-    $("#js-title").show();
-    $("#js-body").hide();
+  $(".panel-body").click(function() {
+    $(this).children("h1").toggle();
+    $(this).children("p").toggle();
   });
 
-  $("#op-title").click(function() {
-    $("#op-title").hide();
-    $("#op-body").show();
-  });
-  $("#op-body").click(function() {
-    $("#op-title").show();
-    $("#op-body").hide();
-  });
-
-  $("#va-title").click(function() {
-    $("#va-title").hide();
-    $("#va-body").show();
-  });
-  $("#va-body").click(function() {
-    $("#va-title").show();
-    $("#va-body").hide();
-  });
-
-  $("#na-title").click(function() {
-    $("#na-title").hide();
-    $("#na-body").show();
-  });
-  $("#na-body").click(function() {
-    $("#na-title").show();
-    $("#na-body").hide();
-  });
-
-  $("#fu-title").click(function() {
-    $("#fu-title").hide();
-    $("#fu-body").show();
-  });
-  $("#fu-body").click(function() {
-    $("#fu-title").show();
-    $("#fu-body").hide();
-  });
-
-  $("#me-title").click(function() {
-    $("#me-title").hide();
-    $("#me-body").show();
-  });
-  $("#me-body").click(function() {
-    $("#me-title").show();
-    $("#me-body").hide();
-  });
-
-  $("#ar-title").click(function() {
-    $("#ar-title").hide();
-    $("#ar-body").show();
-  });
-  $("#ar-body").click(function() {
-    $("#ar-title").show();
-    $("#ar-body").hide();
-  });
-
-  $("#pa-title").click(function() {
-    $("#pa-title").hide();
-    $("#pa-body").show();
-  });
-  $("#pa-body").click(function() {
-    $("#pa-title").show();
-    $("#pa-body").hide();
-  });
-
-  $("#re-title").click(function() {
-    $("#re-title").hide();
-    $("#re-body").show();
-  });
-  $("#re-body").click(function() {
-    $("#re-title").show();
-    $("#re-body").hide();
-  });
+  // $(".panel-body").click(function() {
+  //   if($(this).children("h1").length)
+  //     $(this).children('p').show()
+  //     $(this).children('h1').hide()
+  //   else
+  //     $(this).children('p').hide()
+  //     $(this).children('h1').show()
+  // });
 });
